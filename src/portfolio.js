515e9935492e686d3ddcbba1687e546085e3aa19
nnhra <123168129+Hirozuto@users.noreply.github.com>
2023-09-26 07:15:56 +0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,42 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://itzkaguya.my.id',
+  title: 'Android Builder',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Raka Prasetya Pratama (ItzKaguya)',
+  role: 'Android ROM Builder',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Idk, this just a portfolio sample I got in GitHub',
+  resume: 'https://github.com/nnhra',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    github: 'https://github.com/Hirozuto',
   },
 }
 
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
-  {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
   'HTML',
-  'CSS',
+  'C++',
   'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Kotlin',
+  'Makefile',
+  'Bash',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'hirakinakamura@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
